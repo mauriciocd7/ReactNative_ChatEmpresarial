eb615ce97bb88bacbce3b06fb400c5a48a7d170a
Mauricio Dias <mauriciod.dev@gmail.com>
2022-11-06 16:07:20 -0300

    App.js Updated

---
+++ ChatEmpresarial/src/FirebaseConnection.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAh8nWEVyA5zW3lWDf3r_j-JpAlUnRC7K0",
+    authDomain: "chatempresarial-123dc.firebaseapp.com",
+    projectId: "chatempresarial-123dc",
+    storageBucket: "chatempresarial-123dc.appspot.com",
+    messagingSenderId: "747313791613",
+    appId: "1:747313791613:web:35d65463f6582efac1a714"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;

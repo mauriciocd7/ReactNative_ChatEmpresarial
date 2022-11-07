5c23ba8c506cf875cdb4c50b2d59648b8f0cb286
Mauricio Dias <mauriciod.dev@gmail.com>
2022-11-06 23:57:18 -0300

    Created actions/ reducers/ Reducers.js in /src/

--- ChatEmpresarial/App.js
+++ ChatEmpresarial/App.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { View, Text, Image, ScrollView, TextInput, StyleSheet,  } from 'react-native';
-// Import the functions you need from the SDKs you need
-import firebase from './src/FirebaseConnection';
+import { StackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 
-const App = () => {
-  return (
-    <ScrollView>
-      <Text>Some text</Text>
-      <View>
-        <Text>Some more text</Text>
-        <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{ width: 200, height: 200 }}
-        />
-      </View>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1
-        }}
-        defaultValue="You can type in me"
-      />
-    </ScrollView>
-  );
+import Reducers from './src/Reducers';  /*Os reducers. Cada dado da store deve ter o seu próprio reducer, por exemplo: o dado "user" teria o seu reducer,
+chamado "userReducer". Um reducer é encarregado de lidar com todas as ações, como algum componente pedindo para alterar algum dado da store.*/
+import Preload from './src/Preload';
+import Home from './src/Home';
+
+let store = createStore(Reducers, applyMiddleware(ReduxThunk)); /*"store" é o nome dado pelo Facebook para o conjunto de estados da sua aplicação.
+Vamos pensar na store como um grande centro de informações, que possui disponibilidade para receber e entregar exatamente o que o seu componente requisita (seja uma função, 
+ou uma informação propriamente dita). Tecnicamente, a store é um objeto JavaScript que possui todos os estados dos seus componentes. */
+
+const Navegador = StackNavigator({
+  Preload:{
+    screen:Preload
+  },
+  Home:{
+    screen:Home
+  }
+});
+
+export default class App extends Component {
+  render(){
+    <Provider store = {store}>
+        <Navegador></Navegador>
+    </Provider>
+  }
 }
-
-export default App;

7cd7692f8f632b0221324e3295d942502a600bb0
Mauricio Dias <mauriciod.dev@gmail.com>
2022-11-07 01:45:22 -0300

    AuthReducer.js created, Reducer.js updated

---
+++ ChatEmpresarial/src/reducers/AuthReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    email:'',
+    password:''
+};
+
+const AuthReducer = (state = initialState, action) => { //Constante recebe função anônima
+
+    return state;
+};
+
+export default AuthReducer;
